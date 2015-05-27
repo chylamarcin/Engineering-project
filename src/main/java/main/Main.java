52cc2bafefdb6b43bca5ccbd74f228ba7aabf424
erghar <marcinvip1992@o2.pl>
2015-05-27 16:47:59 +0200

    Start from scratch!

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,12 +5,39 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.encog.Encog;
+import org.encog.engine.network.activation.ActivationSigmoid;
+import org.encog.ml.data.MLData;
+import org.encog.ml.data.MLDataPair;
+import org.encog.ml.data.MLDataSet;
+import org.encog.ml.data.basic.BasicMLDataSet;
+import org.encog.neural.networks.BasicNetwork;
+import org.encog.neural.networks.layers.BasicLayer;
+import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
 
 public class Main extends Application {
 
+    /**
+     * The input necessary for XOR.
+     */
+    public static double XOR_INPUT[][] = { { 0.0, 0.0 }, { 1.0, 0.0 },
+            { 0.0, 1.0 }, { 1.0, 1.0 } };
+
+    /**
+     * The ideal data necessary for XOR.
+     */
+    public static double XOR_IDEAL[][] = { { 0.0 }, { 1.0 }, { 1.0 }, { 0.0 } };
+
+    /**
+     * Main method
+     * @param primaryStage
+     * @throws Exception
+     */
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../../resources/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Backward Propagation");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
@@ -18,6 +45,37 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+        //launch(args);
+        BasicNetwork network = new BasicNetwork();
+        network.addLayer(new BasicLayer(null,true,2));
+        network.addLayer(new BasicLayer(new ActivationSigmoid(),true,3));
+        network.addLayer(new BasicLayer(new ActivationSigmoid(),false,1));
+        network.getStructure().finalizeStructure();
+        network.reset();
+
+        // create training data
+        MLDataSet trainingSet = new BasicMLDataSet(XOR_INPUT, XOR_IDEAL);
+
+        // train the neural network
+        final ResilientPropagation train = new ResilientPropagation(network, trainingSet);
+
+        int epoch = 1;
+
+        do {
+            train.iteration();
+            System.out.println("Epoch #" + epoch + " Error:" + train.getError());
+            epoch++;
+        } while(train.getError() > 0.01);
+        train.finishTraining();
+
+        // test the neural network
+        System.out.println("Neural Network Results:");
+        for(MLDataPair pair: trainingSet ) {
+            final MLData output = network.compute(pair.getInput());
+            System.out.println(pair.getInput().getData(0) + "," + pair.getInput().getData(1)
+                    + ", actual=" + output.getData(0) + ",ideal=" + pair.getIdeal().getData(0));
+        }
+
+        Encog.getInstance().shutdown();
     }
 }
