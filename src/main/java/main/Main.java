d65474cfd1eae6de67bc4369a109a05ba5bff03c
erghar <marcinvip1992@o2.pl>
2015-07-31 21:37:26 +0200

    31 July.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javassist.compiler.SyntaxError;
 import org.encog.Encog;
 import org.encog.engine.network.activation.ActivationSigmoid;
 import org.encog.ml.data.MLData;
@@ -61,22 +62,20 @@ public class Main extends Application {
 
         MLDataSet trainingSet = new BasicMLDataSet(INPUT, IDEAL);
 
-
         // test the neural network
         System.out.println("Neural Network Results:");
 
         double[] helpArray = {14.50, 16.95, 20.90, 5.10};
         double[] error = new double[4];
         int z = 0;
-        for (MLDataPair pair : trainingSet) {
-            final MLData output = network2.compute(pair.getInput());
-            //final MLData output = network.compute(mlData);
-            System.out.printf("Yesterday " + String.format(String.format("%.03f", pair.getInput().getData(3) * helpArray[z]) +
-                    " predicted = " + String.format("%.03f", output.getData(0) * helpArray[z]) +
-                    " correct = " + String.format("%.03f", pair.getIdeal().getData(0) * helpArray[z]) + " difference = " + String.format("%.03f", (output.getData(0) * helpArray[z] - pair.getIdeal().getData(0) * helpArray[z])) + "\n"));
-            z++;
-        }
-
+//        for (MLDataPair pair : trainingSet) {
+//            final MLData output = network2.compute(pair.getInput());
+//            //final MLData output = network.compute(mlData);
+//            System.out.printf("Yesterday " + String.format(String.format("%.04f", pair.getInput().getData(3) * helpArray[z]) +
+//                    " predicted = " + String.format("%.04f", output.getData(0) * helpArray[z]) +
+//                    " correct = " + String.format("%.04f", pair.getIdeal().getData(0) * helpArray[z]) + " difference = " + String.format("%.04f", (output.getData(0) * helpArray[z] - pair.getIdeal().getData(0) * helpArray[z])) + "\n"));
+//            z++;
+//        }
 
 //        int response = JOptionPane.showConfirmDialog(null, "Do you want to save network?", "Confirm",
 //                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
@@ -89,17 +88,7 @@ public class Main extends Application {
 //        }
 //        Encog.getInstance().shutdown();
 
-        // for (int i = 0; i < 10000000; i++) {
-        //  System.out.println(i);
-
         for (int i = 0; i < 10000000; i++) {
-
-
-
-            for (int j = 0; j < 10; j++) {
-                trainNewNetwork(network2, trainingSet);
-                loadAndEvaluate(trainingSet, helpArray, error, network2);
-            }
 
             network2 = new BasicNetwork();
             network2.addLayer(new BasicLayer(null, true, 4));
@@ -109,26 +98,25 @@ public class Main extends Application {
             network2.getStructure().finalizeStructure();
             network2.reset();
 
-
-            System.out.println(i);
+            double[] newNetworkError = trainNewNetwork(network2, trainingSet);
+            loadAndEvaluate(trainingSet, helpArray, newNetworkError, network2);
+            System.out.println("Jeszcze tylko " + (10000000 - i + 1));
         }
-
-        // }
     }
 
-    public static void trainNewNetwork(BasicNetwork network2, MLDataSet trainingSet) {
+    public static double[] trainNewNetwork(BasicNetwork newNetwork, MLDataSet trainingSet) {
 
-        final ResilientPropagation train = new ResilientPropagation(network2, trainingSet);
+        final ResilientPropagation train = new ResilientPropagation(newNetwork, trainingSet);
 
         int epoch = 1;
-        double epochError = 0.00006;
+        double epochError = 0.0000009;
         OutputErrorFunction outputErrorFunction = new OutputErrorFunction();
 
         do {
             train.iteration();
             //System.out.println("Epoch #" + epoch + " Error: " + train.getError());
             epoch++;
-            if (epoch > 15000000) {
+            if (epoch > 200000) {
                 break;
             }
         } while (train.getError() > epochError);
@@ -136,18 +124,21 @@ public class Main extends Application {
         double[] helpArray = {14.50, 16.95, 20.90, 5.10};
         double[] error = new double[4];
         int z = 0;
+        double[] newNetworkError = new double[4];
         for (MLDataPair pair : trainingSet) {
-            final MLData output = network2.compute(pair.getInput());
+            final MLData output = newNetwork.compute(pair.getInput());
             //final MLData output = network.compute(mlData);
-            System.out.printf("Yesterday " + String.format(String.format("%.03f", pair.getInput().getData(3) * helpArray[z]) +
-                    " predicted = " + String.format("%.03f", output.getData(0) * helpArray[z]) +
-                    " correct = " + String.format("%.03f", pair.getIdeal().getData(0) * helpArray[z]) + " difference = " + String.format("%.03f", (output.getData(0) * helpArray[z] - pair.getIdeal().getData(0) * helpArray[z])) + "\n"));
+            System.out.printf("Yesterday " + String.format(String.format("%.04f", pair.getInput().getData(3) * helpArray[z]) +
+                    " predicted = " + String.format("%.04f", output.getData(0) * helpArray[z]) +
+                    " correct = " + String.format("%.04f", pair.getIdeal().getData(0) * helpArray[z]) + " difference = " + String.format("%.04f", (output.getData(0) * helpArray[z] - pair.getIdeal().getData(0) * helpArray[z])) + "\n"));
+            newNetworkError[z] = (output.getData(0) * helpArray[z] - pair.getIdeal().getData(0) * helpArray[z]);
             z++;
         }
 
+        return newNetworkError;
     }
 
-    public static void loadAndEvaluate(MLDataSet trainingSet, double[] helpArray, double[] error, BasicNetwork network2) {
+    public static void loadAndEvaluate(MLDataSet trainingSet, double[] helpArray, double[] newNetworkError, BasicNetwork newNetwork) {
         int z = 0;
         int j = 0;
         System.out.println("Loading network");
@@ -155,37 +146,43 @@ public class Main extends Application {
 
         double e = network.calculateError(trainingSet);
         System.out.println();
-        double[] error2 = new double[4];
+        double[] oldNetworkError = new double[4];
+        System.out.println("----------------old one--------------------");
         for (MLDataPair pair : trainingSet) {
             final MLData output = network.compute(pair.getInput());
             //final MLData output = network.compute(mlData);
-            System.out.printf("Yesterday " + String.format(String.format("%.03f", pair.getInput().getData(3) * helpArray[z]) +
-                    " predicted = " + String.format("%.03f", output.getData(0) * helpArray[z]) +
-                    " correct = " + String.format("%.03f", pair.getIdeal().getData(0) * helpArray[z]) + " difference = " + String.format("%.03f", (output.getData(0) * helpArray[z] - pair.getIdeal().getData(0) * helpArray[z])) + "\n"));
-            error2[z] = (output.getData(0) * helpArray[z] - pair.getIdeal().getData(0) * helpArray[z]);
+            System.out.printf("Yesterday " + String.format(String.format("%.04f", pair.getInput().getData(3) * helpArray[z]) +
+                    " predicted = " + String.format("%.04f", output.getData(0) * helpArray[z]) +
+                    " correct = " + String.format("%.04f", pair.getIdeal().getData(0) * helpArray[z]) + " difference = " + String.format("%.04f", (output.getData(0) * helpArray[z] - pair.getIdeal().getData(0) * helpArray[z])) + "\n"));
+            oldNetworkError[z] = (output.getData(0) * helpArray[z] - pair.getIdeal().getData(0) * helpArray[z]);
             z++;
-
-
-            for (int i = 0; i < error.length; i++) {
-
-                if (error2[i] > error[i] && error[i] >= 0) {
-                    j++;
-                }
-            }
 
         }
 
-        System.out.println(j);
-        if (j >= 3) {
-            EncogDirectoryPersistence.saveObject(new File("network.eg"), network2);
-            System.out.println("WOAH OMG LOL!!!!!!!!!!!!!!!!!!");
-            System.out.println("New network saved");
-            JOptionPane.showMessageDialog(null, "Spam");
+
+        for (int i = 0; i < newNetworkError.length; i++) {
+            if (Math.abs(roundOff(oldNetworkError[i], 3)) >= Math.abs(roundOff(newNetworkError[i], 3))) {
+                j++;
+            }
+        }
+
+        System.err.println("Lepsze wyniki " + j);
+
+        if (j >= 4) {
+            EncogDirectoryPersistence.saveObject(new File("network.eg"), newNetwork);
+            JOptionPane.showMessageDialog(null, "New network saved");
         } else {
             System.out.println("No better network here!");
         }
-
-
     }
 
+    static double roundOff(double x, int position) {
+        double a = x;
+        double temp = Math.pow(10.0, position);
+        a *= temp;
+        a = Math.round(a);
+        return (a / (temp));
+    }
 }
+
+
