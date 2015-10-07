f3a04d33bbca34238cdaea0423b2f91b88e2f85b
erghar <marcinvip1992@o2.pl>
2015-10-07 23:09:15 +0200

    Figured out how to check internet connection. Still need to test it. Tested only without internet.

--- src/main/java/others/Parser.java
+++ src/main/java/others/Parser.java
@@ -1,22 +1,25 @@
 package others;
 
-import dao.DbDao;
-import daoImpl.DbCompany;
-import model.Company;
-import model.CompanyExchange;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+        import dao.DbDao;
+        import daoImpl.DbCompany;
+        import model.Company;
+        import model.CompanyExchange;
+        import org.jsoup.Jsoup;
+        import org.jsoup.nodes.Document;
+        import org.jsoup.nodes.Element;
+        import org.jsoup.select.Elements;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.*;
-import java.text.SimpleDateFormat;
+        import javax.swing.*;
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
+        import java.net.MalformedURLException;
+        import java.net.URL;
+        import java.text.DateFormat;
+        import java.util.*;
+        import java.text.SimpleDateFormat;
+        import java.util.jar.JarOutputStream;
 
 /**
  * Created by odin on 27.05.15.
@@ -45,66 +48,60 @@ public class Parser implements DbDao {
             }
             html = buf.toString();
 
-        } catch (MalformedURLException mue) {
-            mue.printStackTrace();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        } finally {
-            try {
-                if (is != null) is.close();
-            } catch (IOException ioe) {
-                // nothing to see here
-            }
-        }
+            Document doc = Jsoup.parse(html);
 
-        Document doc = Jsoup.parse(html);
+            LinkedList<String> listOfCompanyNames = new LinkedList<String>();
+            LinkedList<String> listOfCompanyValues = new LinkedList<String>();
+            Elements tabElements = doc.getElementsByClass("colWalor");
+            Elements tabElementsCurse  = doc.getElementsByClass("colKurs");
 
-        LinkedList<String> listOfCompanyNames = new LinkedList<String>();
-        LinkedList<String> listOfCompanyValues = new LinkedList<String>();
-        Elements tabElements = doc.getElementsByClass("colWalor");
-        Elements tabElementsCurse = doc.getElementsByClass("colKurs");
 
-        for (int i = 1; i < tabElements.size(); i++) {
-            String companyName = tabElements.get(i).text();
-            String companyValue = tabElementsCurse.get(i).text();
-            listOfCompanyNames.add(companyName);
-            listOfCompanyValues.add(companyValue);
-        }
-
-        Date date = new Date();
-        ArrayList<Company> companies = new ArrayList<Company>();
-
-        for (int i = 0; i < listOfCompanyNames.size(); i++) {
-            Company company = new Company();
-            company.setCompanyName(listOfCompanyNames.get(i));
-            companies.add(company);
-        }
-
-        for (int i = 0; i < listOfCompanyNames.size(); i++) {
-            Boolean companyName = false;
-            companyName = dbCompany.booleanFindCompany(listOfCompanyNames.get(i));
-            if (companyName == false) {
-                dbCompany.saveCompany(companies.get(i));
-            }
-        }
-
-        for (int i = 0; i < listOfCompanyNames.size(); i++) {
-            Company company = dbCompany.findCompany(listOfCompanyNames.get(i));
-
-            CompanyExchange companyExchange = new CompanyExchange();
-            companyExchange.setValue(listOfCompanyValues.get(i));
-            companyExchange.setCompany(company);
-            companyExchange.setDate(date);
-            company.addExchange(companyExchange);
-
-            if (company.getCompanyName() == dbCompany.findCompany(listOfCompanyNames.get(i)).getCompanyName()) {
-                dbCompany.updateCompany(company);
-                dbCompany.saveCompanyExchange(companyExchange);
-            } else {
-                dbCompany.saveCompany(company);
-                dbCompany.saveCompanyExchange(companyExchange);
+            for (int i = 1; i < tabElements.size(); i++) {
+                String companyName = tabElements.get(i).text();
+                String companyValue = tabElementsCurse.get(i).text();
+                listOfCompanyNames.add(companyName);
+                listOfCompanyValues.add(companyValue);
             }
 
+            Date date = new Date();
+
+            ArrayList<Company> companies = new ArrayList<Company>();
+
+            for (int i = 0; i < listOfCompanyNames.size(); i++) {
+                Company company = new Company();
+                company.setCompanyName(listOfCompanyNames.get(i));
+                companies.add(company);
+            }
+
+            for (int i = 0; i < listOfCompanyNames.size(); i++) {
+                Boolean companyName = false;
+                companyName = dbCompany.booleanFindCompany(listOfCompanyNames.get(i));
+                if (companyName == false) {
+                    dbCompany.saveCompany(companies.get(i));
+                }
+            }
+
+            for (int i = 0; i < listOfCompanyNames.size(); i++) {
+                Company company = dbCompany.findCompany(listOfCompanyNames.get(i));
+
+                CompanyExchange companyExchange = new CompanyExchange();
+                companyExchange.setValue(listOfCompanyValues.get(i));
+                companyExchange.setCompany(company);
+                companyExchange.setDate(date);
+                company.addExchange(companyExchange);
+
+                if (company.getCompanyName() == dbCompany.findCompany(listOfCompanyNames.get(i)).getCompanyName()) {
+                    dbCompany.updateCompany(company);
+                    dbCompany.saveCompanyExchange(companyExchange);
+                } else {
+                    dbCompany.saveCompany(company);
+                    dbCompany.saveCompanyExchange(companyExchange);
+                }
+
+            }
+
+        }  catch (IOException ioe) {
+            JOptionPane.showMessageDialog(null, "Nie wykryto po??czenia z internetem.");
         }
 
     }
